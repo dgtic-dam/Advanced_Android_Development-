9a26e3e334f4f363607620d9ecff49acaa3e3ec9
Dannas_Cornell <dannas@protonmail.com>
2019-09-28 13:27:02 -0500

    The project was updated

--- AllMenus/app/src/main/java/mx/unam/tic/docencia/allmenus/SecondActivity.kt
+++ AllMenus/app/src/main/java/mx/unam/tic/docencia/allmenus/SecondActivity.kt
@@ -20,6 +20,7 @@ class SecondActivity : AppCompatActivity(), OnChangeColorBarTabSelected {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+       // setSupportActionBar(secondtoolbar)
 
         val mainAdapter= MainAdapter(initPageTitles(), initFragments(), supportFragmentManager)
         secondViewPager.adapter=mainAdapter
@@ -27,15 +28,11 @@ class SecondActivity : AppCompatActivity(), OnChangeColorBarTabSelected {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
