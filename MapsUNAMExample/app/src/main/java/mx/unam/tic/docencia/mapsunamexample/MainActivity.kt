7226c224c907da3dfbdb25ae510da09861a7d4b9
Dannas_Cornell <dannas@protonmail.com>
2019-10-12 10:28:24 -0500

    The project was updated

--- MapsUNAMExample/app/src/main/java/mx/unam/tic/docencia/mapsunamexample/MainActivity.kt
+++ MapsUNAMExample/app/src/main/java/mx/unam/tic/docencia/mapsunamexample/MainActivity.kt
@@ -21,46 +21,47 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 import com.google.android.gms.tasks.OnSuccessListener
 import kotlinx.android.synthetic.main.activity_main.*
 import mx.unam.tic.docencia.mapsunamexample.dialog.DialogGPS
 
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback,
-    GoogleApiClient.ConnectionCallbacks, OnConnectionFailedListener, LocationListener{
+    GoogleApiClient.ConnectionCallbacks,
+    GoogleApiClient.OnConnectionFailedListener,
+    LocationListener {
 
-    private var mainGoogleMap: GoogleMap?=null
-    private var googleApiClient:GoogleApiClient?= null
-    private var location:Location?=null
-    private var locationManager:LocationManager?=null
-    private var locationCallback:LocationCallback= object : LocationCallback() {
+    private var mainGoogleMap: GoogleMap? = null
+    private var googleApiClient: GoogleApiClient? = null
+    private var location: Location? = null
+    private var locationManager: LocationManager? = null
+    private val locationCallback: LocationCallback = object : LocationCallback() {
         override fun onLocationResult(p0: LocationResult?) {
-            onLocationResult(p0?.lastLocation)
+            onLocationChanged(p0?.lastLocation)
         }
     }
-    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-    private var locationRequest:LocationRequest?=null
-    private var locationManagerAux:LocationManager?=null
 
-    private val isLocationEnabled:Boolean
-    get() {
-        locationManagerAux = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        return locationManagerAux!!.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
-                locationManagerAux!!.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
-    }
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private var locationRequest: LocationRequest? = null
+    private var locationManagerAux: LocationManager? = null
+
+    private val isLocationEnabled: Boolean
+        get() {
+            locationManagerAux = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            return locationManagerAux!!.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                    locationManagerAux!!.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+        }
 
     override fun onStart() {
         super.onStart()
-        if (googleApiClient!=null)
-            googleApiClient!!.context
+        if (googleApiClient != null)
+            googleApiClient!!.connect()
     }
 
     override fun onStop() {
         super.onStop()
-        if (googleApiClient!=null && googleApiClient!!.isConnected)
+        if (googleApiClient != null && googleApiClient!!.isConnected)
             googleApiClient!!.disconnect()
     }
 
@@ -69,16 +70,25 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        val mainMapFragment=supportFragmentManager.findFragmentById(R.id.mainMapFragment)
+        val mainMapFragment = supportFragmentManager
+            .findFragmentById(R.id.mainMapFragment)
                 as SupportMapFragment
         mainMapFragment.getMapAsync(this)
 
-        googleApiClient=GoogleApiClient.Builder(this).apply {
-            addConnectionCallbacks(this@MainActivity) //reciben la implementación de la interface, de tipo MainActivity
-            addOnConnectionFailedListener(this@MainActivity) // ==MainActivity.class
+        googleApiClient = GoogleApiClient.Builder(this).apply {
+            addConnectionCallbacks(this@MainActivity)
+            addOnConnectionFailedListener(this@MainActivity)
             addApi(LocationServices.API)
         }.build()
-        checkPermissions()
+
+        if(checkPermissions())
+            startLocation()
+
+        locationFloatingActionButton.setOnClickListener{
+            if (checkPermissions())
+                startLocation()
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -102,28 +112,29 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        if (requestCode==PERMISSION_GPS_GRANTED){
-            if (grantResults.size>0){
-                if (grantResults[0]==PackageManager.PERMISSION_GRANTED){
+        if (requestCode == PERMISSION_GPS_GRANTED) {
+            if (grantResults.size > 0) {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     startLocation()
-                }else{
-                    Toast.makeText(this, "Requiere aceptar los permisos",
-                    Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this,"Requieres aceptar los permisos",
+                        Toast.LENGTH_LONG).show()
                 }
-            }else{
-                Toast.makeText(this, "Requiere aceptar los permisos",
+            } else {
+                Toast.makeText(this,"Requieres aceptar los permisos",
                     Toast.LENGTH_LONG).show()
             }
-        }else{
+        } else {
             super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         }
+
     }
 
-    override fun onMapReady(p0: GoogleMap?) { //implementando mapas
-        mainGoogleMap=p0
-        mainGoogleMap!!.uiSettings.isZoomGesturesEnabled=true
-        mainGoogleMap!!.uiSettings.isZoomControlsEnabled=true
-        mainGoogleMap!!.uiSettings.isMyLocationButtonEnabled=true
+    override fun onMapReady(p0: GoogleMap?) {
+        mainGoogleMap = p0
+        mainGoogleMap!!.uiSettings.isZoomGesturesEnabled = true
+        mainGoogleMap!!.uiSettings.isZoomControlsEnabled = true
+        mainGoogleMap!!.uiSettings.isMyLocationButtonEnabled = true
     }
 
     override fun onConnected(p0: Bundle?) {
@@ -139,108 +150,146 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,
     }
 
     override fun onLocationChanged(p0: Location?) {
-        showLocationMap(p0)
+        showLocationInMap(p0)
     }
 
-    fun checkPermissions():Boolean{
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
-            && ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.ACCESS_COARSE_LOCATION)){
-                val dialogGPS=DialogGPS()
+    fun checkPermissions():Boolean {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+            != PackageManager.PERMISSION_GRANTED
+            && ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ActivityCompat
+                    .shouldShowRequestPermissionRationale(
+                        this, Manifest.permission
+                            .ACCESS_COARSE_LOCATION
+                    )
+            ) {
+                val dialogGPS = DialogGPS()
                 dialogGPS.setOnAcceptClickListener {
                     acceptPermission()
                 }
                 dialogGPS.setOnCancelClickListener {
-                    Toast.makeText(this, "Tienes que aceptar", Toast.LENGTH_LONG).show()
+                    Toast.makeText(
+                        this,
+                        "Tienes que aceptar", Toast.LENGTH_LONG
+                    ).show()
                 }
                 dialogGPS.show(supportFragmentManager, "WARNING-GPS")
                 return false
-            }else{
+            } else {
                 acceptPermission()
                 return false
             }
-        }else{
+        } else {
             return true
         }
     }
 
-    fun acceptPermission(){
-        ActivityCompat.requestPermissions(this, arrayOf(
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.ACCESS_FINE_LOCATION
-        ), PERMISSION_GPS_GRANTED)
+    fun acceptPermission() {
+        ActivityCompat.requestPermissions(
+            this, arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ), PERMISSION_GPS_GRANTED
+        )
     }
 
-    fun initLocation(){
-        locationRequest=LocationRequest.create().apply {
-            priority=LocationRequest.PRIORITY_HIGH_ACCURACY
-            interval= UPDATE_INTERVAL
-            fastestInterval= FASTEST_INTERVAL
+    fun initLocation() {
+        locationRequest = LocationRequest.create().apply {
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            interval = UPDATE_INTERVAL
+            fastestInterval = FASTEST_INTERVAL
         }
 
-        val builder=LocationSettingsRequest.Builder()
+        val builder = LocationSettingsRequest.Builder()
         builder!!.addLocationRequest(locationRequest!!)
-        val locationSettingsRequest=builder.build()
+        val locationSettingsRequest = builder.build()
 
-        val settingsClient=LocationServices.getSettingsClient(this)
+        val settingsClient = LocationServices.getSettingsClient(this)
         settingsClient.checkLocationSettings(locationSettingsRequest)
 
-        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback,Looper.getMainLooper())
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest, locationCallback,
+            Looper.getMainLooper()
+        )
+
     }
 
-    fun startLocation(){
-        if (isLocationEnabled){
-            fusedLocationProviderClient=LocationServices.getFusedLocationProviderClient(this)
-            location=getLastLocation()
-            if (location!=null)
-                showLocationMap(location)
+    fun startLocation() {
+        if(isLocationEnabled) {
+            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+            location = getLastLocation()
+            if (location != null)
+                showLocationInMap(location)
             else
                 initLocation()
         }else{
-            Toast.makeText(this, "Requiere habilitar el GPS",
+            Toast.makeText(this,"Requieres habilitar el GPS",
                 Toast.LENGTH_LONG).show()
         }
+
     }
 
-    fun getLastLocation():Location?{
-        var lastLocation:Location?=null
+    fun getLastLocation(): Location? {
+        var lastLocation: Location? = null
         fusedLocationProviderClient.lastLocation.addOnSuccessListener {
-            OnSuccessListener<Location>{
-                if(it!=null)
-                lastLocation
+            OnSuccessListener<Location> {
+                if (it != null)
+                    lastLocation = it
             }
         }
         return lastLocation
     }
 
-    fun showLocationMap(location: Location?){
-        if (location!=null){
-            val position=LatLng(location.latitude,location.longitude)
+    fun showLocationInMap(location: Location?) {
+        if (location != null) {
+            val position = LatLng(location.latitude, location.longitude)
             val cameraPosition = CameraPosition.Builder()
                 .target(position)
-                .zoom(50f)
-                .bearing(70f)
-                .tilt(25f)
+                .zoom(20f)
+                .bearing(20f)
+                .tilt(10f)
                 .build()
             mainGoogleMap?.let {
-                it.addMarker(MarkerOptions().position(position).title("Usted está aquí"))
-                it.moveCamera(CameraUpdateFactory.newLatLng(position))
-                it.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+                it.addMarker(MarkerOptions()
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.baseline_home_black_18dp))
+                    .position(position).title("Aqui Estas"))
+                //it.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))//mover el mapa sin efecto
+                it.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))//mover el mapa con efecto
             }
             stopLocation()
+        }else{
+            val position = LatLng(0.3223, -0.72372373)
+            val cameraPosition = CameraPosition.Builder()
+                .target(position)
+                .zoom(20f)
+                .bearing(20f)
+                .tilt(10f)
+                .build()
+            mainGoogleMap?.let {
+                it.addMarker(MarkerOptions()
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.baseline_home_black_18dp))
+                    .position(position).title("Aqui Estas"))
+                //it.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))//mover el mapa sin efecto
+                it.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))//mover el mapa con efecto
+            }
         }
     }
 
-    fun stopLocation(){
+    fun stopLocation() {
         fusedLocationProviderClient.removeLocationUpdates(locationCallback)
     }
 
-    companion object{
-        const val PERMISSION_GPS_GRANTED=2321
-        const val UPDATE_INTERVAL=(10*1000).toLong()
-        const val FASTEST_INTERVAL:Long=2000
+    companion object {
+        const val PERMISSION_GPS_GRANTED = 2321
+        const val UPDATE_INTERVAL = (10 * 1000).toLong()
+        const val FASTEST_INTERVAL: Long = 2000
     }
 }
